6b6a24f2d92ba9154f6d3cb47a62269bc726c2a9
M4r1aN24 <142494616+M4r1aN24@users.noreply.github.com>
2023-11-20 15:46:33 +0000

    succesfully showing details of todays city weather conditions got from API call

--- assets/logic.js
+++ assets/logic.js
@@ -1,6 +1,7 @@
-$('#search-button').on('click', function (event) {
-  event.preventDefault()
+$('#search-button').on('click', function displayTodaysWeather(event) {
+  event.preventDefault();
   var city = $(".form-input").val();
+  
     var queryURL =
       'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=e0304d8dfbbc9ab6fcf8b56eddb27564';
     fetch(queryURL)
@@ -8,6 +9,36 @@ $('#search-button').on('click', function (event) {
         return response.json();
       })
       .then(function (data) {
-       console.log(data);
+        var todaysForecast = $("#today");
+        console.log(data);
+      // The city name
+      // The date
+      // An icon representation of weather conditions
+      // The temperature
+      // The humidity
+      // The wind speed
+      // Storing the rating data
+      var todaysDiv = $("<div class = todaysDiv>");
+      todaysForecast.append(todaysDiv);
+
+      var cityName = data.name;
+      var pOne = $("<p>").text("Name: " + cityName + dayjs().format(" (DD/MM/YYYY)")) ;
+      todaysDiv.append(pOne);
+
+    
+
+      var todaysTemperature = data.main.temp -272.15;
+      var pThree = $("<p>").text("Temperature: " + todaysTemperature.toFixed(2) + " Celcius");
+      todaysDiv.append(pThree);
+
+      var todaysHumidity = data.main.humidity;
+      var pFour = $("<p>").text("Humidity: " + todaysHumidity);
+      todaysDiv.append(pFour);
+
+      var todaysWind = data.wind.speed;
+      var pFive = $("<p>").text("Wind speed: " + todaysWind + " miles per hour");
+      todaysDiv.append(pFive);
+
+       
       });
   });
